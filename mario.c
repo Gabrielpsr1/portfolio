f01779925cf0f3e579e27c3c462aa446a48ae2a6
Gabrielpsr1 <Gabrielpsr1@users.noreply.github.com>
2025-03-12 14:01:08 -0300

    automated commit by submit50

--- mario.c
+++ mario.c
@@ -1,36 +1,33 @@
-#include <stdio.h>
 #include <cs50.h>
+#include <stdio.h>
 
 void print_row(int bricks);
 void print_row2(int bricks);
 void print_bs(int bricks);
 
-
 int main(void)
 {
+    // get the user input
     int height;
     do
     {
         height = get_int("What's the height of the pyramid? ");
     }
-    while (height <= 0);
+    while (height <= 0 && height <= 8);
 
-
-    for(int j = 0; j < height; j++)
+    // loop to print
+    for (int j = 0; j < height; j++)
     {
         print_bs(height - j - 1);
         print_row(j + 1);
         print_bs(2);
         print_row2(j + 1);
     }
-
-
-
 }
 
 void print_row(int bricks)
 {
-    for(int i = 0; i < bricks; i++)
+    for (int i = 0; i < bricks; i++)
     {
         printf("#");
     }
@@ -38,7 +35,7 @@ void print_row(int bricks)
 
 void print_row2(int bricks)
 {
-    for(int i = 0; i < bricks; i++)
+    for (int i = 0; i < bricks; i++)
     {
         printf("#");
     }
@@ -48,7 +45,7 @@ void print_row2(int bricks)
 // bs is blank space, not bs (you know what i mean)
 void print_bs(int bricks)
 {
-    for(int i = 0; i < bricks; i++)
+    for (int i = 0; i < bricks; i++)
     {
         printf(" ");
     }
