0f1457f18bd77a56359129642f22fa199465823f
Gabrielpsr1 <Gabrielpsr1@users.noreply.github.com>
2025-03-18 10:02:36 -0300

    automated commit by check50 [check50=True]

---
+++ substitution.c
@@ -0,0 +1,49 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+bool only_letters(string key);
+string cipher(string text,string key);
+
+int main(int argc, string argv[])
+{
+    string key = argv[1];
+
+    if(argc != 2 || only_letters(key))
+    {
+        printf("Usage: ./substitution key\n");
+        return 1;
+    }
+    if(strlen(key) != 26)
+    {
+        printf("Key must have 26 letters.\n");
+        return 3;
+    }
+    string text = get_string("plaintext: ");
+    printf("ciphertext: %s\n", cipher(text,key));
+}
+
+
+bool only_letters(string key)
+{
+    for(int i = 0,len = strlen(key); i<len;i++)
+    {
+        if(!isalpha(key[i]))
+        {
+            return 2;
+        }
+    }
+    return 0;
+}
+
+string cipher(string text,string key)
+{
+    for(int i=0,len=strlen(text); i < len; i++)
+    {
+        int ci = text[i] - 'a';
+        text[i] = key[ci];
+    }
+    return text;
+}
