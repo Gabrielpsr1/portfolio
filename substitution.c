2beea1651dde6921ea8c52e26e5fecd50e0843bf
Gabrielpsr1 <Gabrielpsr1@users.noreply.github.com>
2025-03-18 12:43:17 -0300

    automated commit by check50 [check50=True]

--- substitution.c
+++ substitution.c
@@ -6,46 +6,46 @@
 
 bool only_letters(string key);
 bool duplicate_letters(string key);
-string cipher(string text,string key);
+string cipher(string text, string key);
 
 int main(int argc, string argv[])
 {
     string key = argv[1];
 
-    if(argc != 2 || only_letters(key))
+    if (argc != 2 || only_letters(key))
     {
         printf("Usage: ./substitution key\n");
         return 1;
     }
-    if(strlen(key) != 26 || duplicate_letters(key))
+    if (strlen(key) != 26 || duplicate_letters(key))
     {
         printf("Key must have 26 different letters.\n");
         return 1;
     }
     string text = get_string("plaintext: ");
-    printf("ciphertext: %s\n", cipher(text,key));
+    printf("ciphertext: %s\n", cipher(text, key));
 }
 
 bool duplicate_letters(string key)
 {
-    int ascii = 2015;
-    for(int i = 0,len = strlen(key); i<len;i++)
+    for (int i = 0, len = strlen(key); i < len; i++)
     {
-        key[i] = toupper(key[i]);
-        ascii -= key[i];
-    }
-    if(ascii != 0)
-    {
-        return 2;
+        for (int j = i + 1; i < len; j++)
+        {
+            if (key[i] == key[j])
+            {
+                return 1;
+            }
+        }
     }
     return 0;
 }
 
 bool only_letters(string key)
 {
-    for(int i = 0,len = strlen(key); i<len;i++)
+    for (int i = 0, len = strlen(key); i < len; i++)
     {
-        if(!isalpha(key[i]))
+        if (!isalpha(key[i]))
         {
             return 2;
         }
@@ -53,22 +53,22 @@ bool only_letters(string key)
     return 0;
 }
 
-string cipher(string text,string key)
+string cipher(string text, string key)
 {
     int ci;
-    for(int i=0,len=strlen(text); i < len; i++)
+    for (int i = 0, len = strlen(text); i < len; i++)
     {
-        if(islower(text[i]))
+        if (islower(text[i]))
         {
-             ci = text[i] - 'a';
-             key[ci] = tolower(key[ci]);
-             text[i] = key[ci];
+            ci = text[i] - 'a';
+            key[ci] = tolower(key[ci]);
+            text[i] = key[ci];
         }
-        if(isupper(text[i]))
+        if (isupper(text[i]))
         {
-             ci = text[i] - 'A';
-             key[ci] = toupper(key[ci]);
-             text[i] = key[ci];
+            ci = text[i] - 'A';
+            key[ci] = toupper(key[ci]);
+            text[i] = key[ci];
         }
     }
     return text;
