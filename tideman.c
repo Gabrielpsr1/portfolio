90fbe8abfdff9fd9b0ac41f415f910c905749747
Gabrielpsr1 <Gabrielpsr1@users.noreply.github.com>
2025-03-26 13:17:18 -0300

    automated commit by check50 [check50=True]

--- tideman.c
+++ tideman.c
@@ -226,15 +226,20 @@ void print_winner(void)
     // TODO estabilizar a coluna e ciclar as linha procurando qual n tem falso
     for (int i = 0; i < candidate_count; i++)
     {
+        bool print = true;
         for (int j = 0; j < candidate_count; j++)
         {
             if (locked[j][i])
             {
+                print = false;
                 break;
             }
         }
+        if(print)
+        {
             printf("%s\n", candidates[i]);
 
+        }
     }
     return;
 }
