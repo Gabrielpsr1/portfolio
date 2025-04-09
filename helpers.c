54e22cf0baa0593cc147b006aa7b80a7d7e6548b
Gabrielpsr1 <Gabrielpsr1@users.noreply.github.com>
2025-04-09 11:42:41 -0300

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -30,7 +30,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
     for(int i = 0; i<height; i++)
     {
         // cicling the coloums
-        for(int j = 0; j<width; j++)
+        for(int j = 0; j<width/2 ; j++)
         {
             temp = image[i][width - (j+1)];
             image[i][width - (j+1)] = image[i][j];
