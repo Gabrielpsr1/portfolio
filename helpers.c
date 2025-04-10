05519999737f1c2dffe8ca85c618167427588d78
Gabrielpsr1 <Gabrielpsr1@users.noreply.github.com>
2025-04-09 23:25:20 -0300

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -79,9 +79,9 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                     number_m++;
                 }
             }
-            temp[i][j].rgbtBlue = sum_blue / number_m*1.0;
-            temp[i][j].rgbtGreen = sum_green / number_m*1.0;
-            temp[i][j].rgbtRed = sum_red / number_m*1.0;
+            temp[i][j].rgbtBlue = (int)round(sum_blue / number_m);
+            temp[i][j].rgbtGreen = (int)round(sum_green / number_m);
+            temp[i][j].rgbtRed = (int)round(sum_red / number_m);
         }
     }
     for(int i = 0; i < height; i++)
