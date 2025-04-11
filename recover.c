d758e9796dbda8aa327ca84aa5fe4d5a73e76ebb
Gabrielpsr1 <Gabrielpsr1@users.noreply.github.com>
2025-04-11 11:10:27 -0300

    automated commit by check50 [check50=True]

--- recover.c
+++ recover.c
@@ -18,6 +18,7 @@ int main(int argc, char *argv[])
     FILE *card = fopen(argv[1], "r");
     if(card == NULL)
     {
+        free(card);
         return 2;
     }
     // repeat until end of the file
@@ -35,6 +36,7 @@ int main(int argc, char *argv[])
                 img =fopen(filename,"w");
                 if(img == NULL)
                 {
+                    free(img);
                     return 2;
                 }
                 fwrite(buffer,sizeof(uint8_t),512,img);
@@ -48,6 +50,7 @@ int main(int argc, char *argv[])
                 img = fopen(filename, "w");
                 if(img == NULL)
                 {
+                    free(img);
                     return 2;
                 }
                 fwrite(buffer,sizeof(uint8_t),512,img);
