5532b1261e1566238f06466cdea8e9780302eb49
Gabrielpsr1 <Gabrielpsr1@users.noreply.github.com>
2025-04-17 15:01:07 -0300

    automated commit by check50 [check50=True]

--- dictionary.c
+++ dictionary.c
@@ -75,7 +75,7 @@ bool load(const char *dictionary)
         n = malloc(sizeof(node));
         {
             if (n == NULL)
-            // fclose(file);
+            fclose(file);
             return false;
         }
 
