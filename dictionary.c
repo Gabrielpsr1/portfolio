f87b0306659db81d09c34f048369e345730b23ee
Gabrielpsr1 <Gabrielpsr1@users.noreply.github.com>
2025-04-17 14:39:27 -0300

    automated commit by check50 [check50=True]

--- dictionary.c
+++ dictionary.c
@@ -26,35 +26,49 @@ int word_count = 0;
 // Returns true if word is in dictionary, else false
 bool check(const char *word)
 {
+    // unsigned int key = hash(word);
+    // node *n = malloc(sizeof(node));
+
+    // // if its on the first element of the list
+    // if(table[key] == NULL)
+    // return false;
+
+    // if (strcasecmp(table[key]->word, word) == 0)
+    // {
+    //     return true;
+    // }
+    // else if (table[key] == NULL)
+    // {
+    //     return false;
+    // }
+    // else
+    // {
+    //     n = table[key]->next;
+    //     while (n != NULL)
+    //     {
+    //         if (strcasecmp(n->word, word) == 0)
+    //             return true;
+
+    //         else
+    //             n = n->next;
+    //     }
+    // }
+    // return false;
     unsigned int key = hash(word);
-    node *n = malloc(sizeof(node));
+    node *cursor = table[key];
 
-    // if its on the first element of the list
-    if(table[key] == NULL)
-    return false;
-
-    if (strcasecmp(table[key]->word, word) == 0)
+    while (cursor != NULL)
     {
-        return true;
-    }
-    else if (table[key] == NULL)
-    {
-        return false;
-    }
-    else
-    {
-        n = table[key]->next;
-        while (n != NULL)
+        if (strcasecmp(cursor->word, word) == 0)
         {
-            if (strcasecmp(n->word, word) == 0)
-                return true;
-
-            else
-                n = n->next;
+            return true;
         }
+        cursor = cursor->next;
     }
     return false;
 }
+
+
 
 // Hashes word to a number
 unsigned int hash(const char *word)
@@ -98,7 +112,7 @@ bool load(const char *dictionary)
             buffer[i] = toupper(buffer[i]);
         }
         strcpy(n->word, buffer);
-        
+
         n->next = NULL;
         hashk = hash(n->word);
 
