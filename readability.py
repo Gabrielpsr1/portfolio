e673c0790b214edd52325bf39a6d86d03536696c
Gabrielpsr1 <Gabrielpsr1@users.noreply.github.com>
2025-04-28 14:29:34 -0300

    automated commit by check50 [check50=True]

--- readability.py
+++ readability.py
@@ -1,4 +1,4 @@
-def printf(index)
+def printf(index):
     if index < 1:
         print("Before Grade 1")
     elif index > 16:
@@ -8,39 +8,39 @@ def printf(index)
 
 # count words
 
-def words(string)
+def words(string):
     words_count = 1
     for i in range(len(string)):
-        if string[i] == " "
+        if string[i] == " ":
            words_count += 1
     return words_count
 
 # count letters
 
-def letters(string)
+def letters(string):
     letters_count = 0
     for i in range(len(string)):
-        if string[i].isalpha()
+        if string[i].isalpha():
            letters_count += 1
     return letters_count
 
 
-def sentences(string)
+def sentences(string):
     sentences_count = 0
-    for i in range(len(string))
-        if string[i] in ('.', '!','?')
+    for i in range(len(string)):
+        if string[i] in ('.', '!','?'):
            sentences_count += 1
     return sentences_count
 
 
-def main()
-   str string = input("text: ")
+def main():
+    string = input("text: ")
     # calculate the index
     l = letters(string) * 100 / words(string)
     s = sentences(string) * 100 / words(string)
     index = 0.0588 * l - 0.296 * s - 15.8
 
-    print(index)
+    printf(index)
 
 
 main()
