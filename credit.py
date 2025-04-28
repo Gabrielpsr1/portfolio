ad82ceec617faadf7e75e379067e9ccac1819c55
Gabrielpsr1 <Gabrielpsr1@users.noreply.github.com>
2025-04-28 13:05:14 -0300

    automated commit by check50 [check50=True]

--- credit.py
+++ credit.py
@@ -9,13 +9,13 @@ def valid(cardnumber):
     count = 0
     if lenght not in (13, 15, 16):
         return False
-    for i in range(lenght - 1, 0, 2):
+    for i in range(lenght - 1, 0, -2):
         if i * 2 > 9:
             count += (i*2)/10 + (i*2) % 10
         else:
             count += i*2
 
-    for i in range(lenght, 0, 2):
+    for i in range(lenght, 0, -2):
         count += i
 
     if count % 10 == 0:
@@ -30,7 +30,7 @@ def flag(cardnumber):
         return "AMEX"
     elif 51 <= int(str(cardnumber)[:2]) <= 55:
         return "MASTERCARD"
-    elif 40 <= int(str(cardnumber)[:2]) <= 49 and len(str(cardnumber)) in (13,16):
+    elif str(cardnumber)[:1] == "4" and len(str(cardnumber)) in (13,16):
         return "VISA"
     else:
         return "INVALID"
@@ -40,3 +40,5 @@ if valid(card) == True:
     print(f"{flag(card)}")
 if valid(card) == False:
     print("INVALID")
+else:
+    print("valid")
