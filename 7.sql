edcfcaed5798d5717bd1c2a7f38abfc48b4a90e9
Gabrielpsr1 <Gabrielpsr1@users.noreply.github.com>
2025-07-18 15:48:02 -0300

    automated commit by check50 [check50=True]

--- 7.sql
+++ 7.sql
@@ -1,1 +1,1 @@
-SELECT title,rating FROM movies JOIN ratings on id = movie_id WHERE year = 2010 ORDER BY rating,title;
+SELECT title,rating FROM movies JOIN ratings on id = movie_id WHERE year = 2010 ORDER BY rating(DESC),title;
