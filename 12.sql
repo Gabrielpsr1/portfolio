2cfd161bd6676569c10a7a5a431b0734154df136
Gabrielpsr1 <Gabrielpsr1@users.noreply.github.com>
2025-07-18 15:55:16 -0300

    automated commit by check50 [check50=True]

--- 12.sql
+++ 12.sql
@@ -1,4 +1,5 @@
 SELECT title FROM movies
 WHERE id IN (SELECT movie_id FROM stars
-WHERE person_id IN (SELECT id FROM people WHERE name = "Bradley Cooper") AND (SELECT id FROM people WHERE name = "Jennifer Lawrence"))
+WHERE person_id IN (SELECT id FROM people WHERE name = "Bradley Cooper")) AND (SELECT movie_id FROM stars
+WHERE person_id IN (SELECT id FROM people WHERE name = "Jennifer Lawrence"))
 ORDER BY title;
